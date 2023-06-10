925f240dfc065ce45149769f2951effdac796054
580dev <dansteve580@gmail.com>
2023-06-11 00:51:29 +0200

    manage user in localstorage

---
+++ src/app/app-service/app.service.ts
@@ -0,0 +1,98 @@
+import { Injectable } from '@angular/core';
+
+export enum ROLE_USER {
+  BDE,STUDENT
+}
+
+export interface Interet {
+  id: number,
+  libelle: string
+}
+
+export interface User {
+  id: number,
+  name?: string,
+  email: string,
+  password: string,
+  role: ROLE_USER,
+  interet: Interet[]
+}
+
+export interface Event {
+  id: number,
+  titre: string,
+  description: string,
+  date: string,
+  image?: string
+}
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  constructor() { }
+
+  saveUsers() {
+    const users: User[] = [
+      {
+        id: 1,
+        email: 'pierre@sorbon.fr',
+        role: ROLE_USER.BDE,
+        password: '1234',
+        name: 'Pierre Henry',
+        interet: [],
+      },
+      {
+        id: 2,
+        email: 'marie@paris.fr',
+        role: ROLE_USER.STUDENT,
+        password: '5678',
+        name: 'Marie Dupont',
+        interet: [],
+      },
+      {
+        id: 3,
+        email: 'john@doe.com',
+        role: ROLE_USER.STUDENT,
+        password: 'abcd',
+        name: 'John Doe',
+        interet: [],
+      }
+    ];
+
+    const localUsers: User[] = this.getUsers()
+    if(localUsers.length === 0){
+      localStorage.setItem("users", JSON.stringify(users))
+    }
+  }
+
+  getUsers(): User[] {
+    const users = localStorage.getItem('users')
+    if(users) {
+      return JSON.parse(users)
+    } else {
+      return [] as User[]
+    }
+  }
+
+  addInteretsToUser(interts: Interet[], userId: number) {
+    let user: User = this.findUser(userId)!
+    user.interet = interts
+    this.putUser(user)
+  }
+
+  findUser(id: number) {
+    const users = this.getUsers()
+    return users.find(user => user.id === id)
+  }
+
+  putUser(user: User) {
+    let users = this.getUsers()
+    let userIndex = users.findIndex(userItem => user.id === userItem.id)
+    users[userIndex] = user
+    localStorage.setItem('users', JSON.stringify(users))
+  }
+
+}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,14 +7,12 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-  email!: string;
-  password!: string;
+  email?: string;
+  password?: string;
 
   constructor(private router: Router){}
 
   login() {
-    console.log('email:', this.email);
-    console.log('Mot de passe:', this.password);
     if(this.email === "admin@linkHub.fr" && this.password === "admin") {
       this.router.navigate(['admin/list-event'])
     }
